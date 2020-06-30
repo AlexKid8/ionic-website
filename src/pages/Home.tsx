e6ccc1c5605f310e7ddfe47d60b8f194eb494abf
AlexKid8 <alexkid8@gmail.com>
2020-06-30 23:32:05 +0200

    Modify Page Title

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,7 +8,7 @@ const Home: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>Alexandre Ben el Hiouel</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent>
